d0e5108f7a20c2de30b7b2d4f334467b0336779d
zahidhasan125 <zahidhasan125@gmail.com>
2022-10-25 10:24:26 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello from learning platform')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+})
