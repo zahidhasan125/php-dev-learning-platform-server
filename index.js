9e1175347fe29d8b7fa9857ae5e33e04d97e3b1d
zahidhasan125 <zahidhasan125@gmail.com>
2022-10-26 03:18:41 -0400

    course data send with dynamic response

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const courseTopic = require('./course-topic.json');
+const coursesDetails = require('./course-details.json');
 
 app.use(cors());
 
@@ -13,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
     res.send(courseTopic);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const courseData = coursesDetails.find(course => course.courseId == parseInt(id));
+    res.send(courseData);    
 })
 
 app.listen(port, () => {
