b65669c415706fcea66c83f9759e56ec46fcfaf8
zahidhasan125 <zahidhasan125@gmail.com>
2022-10-26 01:12:54 -0400

    course topic data added to /courses

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const courseTopic = require('./course-topic.json');
+
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello from learning platform')
 })
 
-
+app.get('/courses/', (req, res) => {
+    res.send(courseTopic);
+})
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
